2b6abc2f8ca1acac7642c7625466dec76116249b
Igor Souza <88636711+igorsouzaccruz@users.noreply.github.com>
2024-05-30 12:39:04 -0300

    ajuste rotas

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        children : [{
+            path: '', redirectTo : 'login', pathMatch:'prefix'
+        },
+    {
+        path:'login',
+        title: 'Login',
+        loadComponent: () => import('./login/login.component').then((c) => c.LoginComponent)
+    },
+{
+    path:'usuario',
+    title:'Usuário',
+    loadChildren: () => import('./usuario/usuario.routes').then((c) => c.default)
+}]
+    }
+];
--- front/src/app/usuario/usuario.routes.ts
+++ front/src/app/usuario/usuario.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 import { UsuarioComponent } from './usuario.component';
+import { ListagemComponent } from './listagem/listagem.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 export default [
   {
